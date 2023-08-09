a77a91da3e94480af1946e7725d641039342d5dd
brian <brian.thiely@gmail.com>
2023-08-09 16:16:53 +0200

    Change fixtures

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -14,13 +14,21 @@ class TaskFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        for ($i = 0; $i < 20; $i++) {
+        for ($i = 0; $i < 5; $i++) {
             $task = new Task();
             $task->setTitle('Tâche n°' . $i);
             $task->setContent('Contenu de la tâche n°' . $i);
-            $manager->persist($task);
         }
 
+        $task->setTitle('Task User')
+            ->setContent('OK')
+            ->setUser($this->getReference('user'));
+
+        $task->setTitle('Task User 2')
+            ->setContent('OK')
+            ->setUser($this->getReference('user2'));
+
+        $manager->persist($task);
         $manager->flush();
     }
 }
