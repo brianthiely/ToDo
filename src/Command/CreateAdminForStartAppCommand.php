fe2c259d24b40529a92cc1c4b24acd553c229434
brian <brian.thiely@gmail.com>
2023-08-25 15:51:15 +0200

    Change name command

--- src/Command/CreateAdminForStartAppCommand.php
+++ src/Command/CreateAdminForStartAppCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
-    name: 'CreateAdminForStartApp',
+    name: 'create:admin',
     description: 'Add a short description for your command',
 )]
 class CreateAdminForStartAppCommand extends Command
